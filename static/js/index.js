689856d1193fd8e1e0c0533bfd2b614e0d236319
lifuhao <lfh23@mails.tsinghua.edu.cn>
2026-05-10 21:10:40 +0800

    init

---
+++ static/js/index.js
@@ -0,0 +1,10 @@
+window.HELP_IMPROVE_VIDEOJS = false;
+
+$(document).ready(function() {
+  $(".navbar-burger").click(function() {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
+  });
+
+  bulmaSlider.attach();
+});
